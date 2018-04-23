1c08af96fc6799568a18d5a080bb2f292903a575
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/StateIndicator/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace StateIndicator {
+    public partial class MainPage : UserControl {
+        public MainPage() {
+            InitializeComponent();
+        }
+
+        private void stateIndicatorControl1_MouseEnter(object sender, MouseEventArgs e) {
+            stateIndicatorControl1.StateIndex = 0;
+        }
+
+        private void stateIndicatorControl1_MouseLeave(object sender, MouseEventArgs e) {
+            stateIndicatorControl1.StateIndex = 2;
+        }
+    }
+}
---
+++ VB/StateIndicator/MainPage.xaml.vb
@@ -0,0 +1,20 @@
+﻿Imports Microsoft.VisualBasic
+Imports System.Windows.Controls
+Imports System.Windows.Input
+
+Namespace StateIndicator
+	Partial Public Class MainPage
+		Inherits UserControl
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+
+		Private Sub stateIndicatorControl1_MouseEnter(ByVal sender As Object, ByVal e As MouseEventArgs)
+			stateIndicatorControl1.StateIndex = 0
+		End Sub
+
+		Private Sub stateIndicatorControl1_MouseLeave(ByVal sender As Object, ByVal e As MouseEventArgs)
+			stateIndicatorControl1.StateIndex = 2
+		End Sub
+	End Class
+End Namespace
